3795397e010b524e9f5c885b6467b50f4f08396a
Kazım İncebay <kazimincebay@gmail.com>
2025-07-29 19:13:36 +0300

    Project developing
    - I created appbar and toolbar in project with Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div>
+<AppBar>
+  <Toolbar sx={{fontSize:30}}>Kart Projesi</Toolbar>
+</AppBar>
+  </div>;
 }
 
 export default App;
