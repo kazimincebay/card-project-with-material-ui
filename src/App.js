57e06c18dfbc7b28b7015bce7e91c77b31795820
Kazım İncebay <kazimincebay@gmail.com>
2025-07-29 19:52:55 +0300

    Project is finished
    - I created course component
    - I added grid in this project
    - I added card in this project

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import "./App.css";
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import Course from "./Course";
+import angular from "./images/angular.jpg";
+import bootstrap from "./images/bootstrap5.png";
+import cCsharp from "./images/ccsharp.png";
+import kompleWeb from "./images/kompleweb.jpg";
 
 function App() {
-  return <div>
-<AppBar>
-  <Toolbar sx={{fontSize:30}}>Kart Projesi</Toolbar>
-</AppBar>
-  </div>;
+  return (
+    <div>
+      <AppBar>
+        <Toolbar sx={{ fontSize: 30 }}>Kart Projesi</Toolbar>
+      </AppBar>
+
+      <Container maxWidth={"lg"}>
+        <Grid container spacing={2} sx={{ marginTop: 10 }}>
+          <Grid item size={{xs:12,md:6,lg:3}}>
+            <Course
+              image={angular}
+              title="Angular"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consequuntur ipsam quam esse, deleniti suscipit ullam, praesentium maxime sequi minus excepturi rerum adipisci! Aliquid et dolore nesciunt distinctio, sed quo?"
+            ></Course>
+          </Grid>
+          <Grid item size={{xs:12,md:6,lg:3}} >
+            <Course
+              image={bootstrap}
+              title="Bootstrap"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consequuntur ipsam quam esse, deleniti suscipit ullam, praesentium maxime sequi minus excepturi rerum adipisci! Aliquid et dolore nesciunt distinctio, sed quo?"
+            ></Course>
+          </Grid>
+          <Grid item size={{xs:12,md:6,lg:3}} >
+            <Course
+              image={cCsharp}
+              title="Csharp"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consequuntur ipsam quam esse, deleniti suscipit ullam, praesentium maxime sequi minus excepturi rerum adipisci! Aliquid et dolore nesciunt distinctio, sed quo?"
+            ></Course>
+          </Grid>
+          <Grid item size={{xs:12,md:6,lg:3}}>
+            <Course
+              image={kompleWeb}
+              title="KompleWeb"
+              description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam consequuntur ipsam quam esse, deleniti suscipit ullam, praesentium maxime sequi minus excepturi rerum adipisci! Aliquid et dolore nesciunt distinctio, sed quo?"
+            ></Course>
+          </Grid>
+        </Grid>
+      </Container>
+    </div>
+  );
 }
 
 export default App;
--- src/Course.js
+++ src/Course.js
@@ -1,5 +1,27 @@
-function Course() {
-    return ( <div></div> );
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+
+function Course({image,title,description}) {
+    return ( <div>
+        <Card>
+      <CardMedia
+        sx={{ height: 140 }}
+        image={image}
+        title={title}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {description}
+        </Typography>
+      </CardContent>
+    </Card>
+    </div> );
 }
 
 export default Course;
